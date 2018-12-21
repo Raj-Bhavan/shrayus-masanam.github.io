8da2ef1f1bb5045ec458f53ddc7c3c6d887867e5
Shrayus <shrayus@gmail.com>
2018-12-20 20:49:59 -0500

    Update keypad.js

--- js/keypad.js
+++ js/keypad.js
@@ -1,9 +1,7 @@
-function rawr {
 var passStore = prompt("Enter your hotword:");
 if (passStore === "zund") {
 	window.open("http://loosingbraincells.ml/otherPages/Zund.html");
 	else if (passStore === "brainpop") {
 		window.open("https://docs.google.com/document/d/1-1enyFQYu85HPXwXQHQIhzQglUKzWEA3uC9hHBm6MNY/edit?usp=sharing");
 	}
-}
 }

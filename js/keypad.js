aaaf82169202aabc11338660edf696897573877c
Shrayus <shrayus@gmail.com>
2019-01-02 17:19:54 -0500

    Delete keypad.js

--- js/keypad.js
+++
@@ -1,7 +0,0 @@
-var passStore = prompt("Enter your hotword:");
-if (passStore === "zund") {
-	window.open("http://loosingbraincells.ml/otherPages/Zund.html");
-	else if (passStore === "brainpop") {
-		window.open("https://docs.google.com/document/d/1-1enyFQYu85HPXwXQHQIhzQglUKzWEA3uC9hHBm6MNY/edit?usp=sharing");
-	}
-}

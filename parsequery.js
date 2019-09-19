de428615633c681c937feb779b5cf1606c3024ad
Shrayus <shrayus@gmail.com>
2019-09-19 17:57:18 -0400

    Link error

--- parsequery.js
+++ parsequery.js
@@ -1,7 +1,7 @@
 window.onload = function() {
 	var link = window.location.href;
 	var content = link.split("=").pop();
-	if (content == "shrayus-masanam.github.io") {
+	if (content == "https://shrayus-masanam.github.io/") {
 		document.body.innerHTML = "<h1>Nothing to display.</h1>"
 	} else {
 		document.body.innerHTML = "<h1>String Generated</h1><br><br>" + content;

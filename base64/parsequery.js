8c2a68f9d4ccff33be9d20c7fc7f7153df036e0c
Shrayus <shrayus@gmail.com>
2019-09-19 20:33:43 -0400

    Fixes "="

--- base64/parsequery.js
+++ base64/parsequery.js
@@ -1,6 +1,6 @@
 window.onload = function() {
 	var link = window.location.href;
-	var content = link.split("=").pop();
+	var content = link.split("=")[0].pop();
 	if (content == "https://shrayus-masanam.github.io/base64/") {
 		document.body.innerHTML = "<h1>Nothing to display.</h1>"
 	} else {

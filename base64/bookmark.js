8ab96d0c1ca791f0b71aa243f20d4c654529d6ef
shrayus-masanam <45981228+shrayus-masanam@users.noreply.github.com>
2019-09-20 16:27:03 -0400

    Update bookmark.js

--- base64/bookmark.js
+++ base64/bookmark.js
@@ -4,11 +4,11 @@ var choice = confirm('Press OK for Encode, Cancel for Decode');
 if (choice==true) { 
   var string = prompt('String?'); 
   var encodedString = btoa(string);
-  document.body.innerHTML = "<body><h1>String Generated</h1><br>" + string + "<br><br><button onclick="document.body.innerHTML = pageCode; delete pageCode;">Restore page</button>";
+  document.body.innerHTML = "<body><h1>String Generated</h1><br>" + string + "<br><br><button onclick='document.body.innerHTML = pageCode; delete pageCode;'>Restore page</button>";
   /* location.replace("https://shrayus-masanam.github.io/base64/?s=*"+encodedString); */ 
 } else { 
   var string = prompt('String?'); 
   var decodedString = atob(string);
-  document.body.innerHTML = "<body><h1>String Generated</h1><br>" + string + "<br><br><button onclick="document.body.innerHTML = pageCode; delete pageCode;">Restore page</button>";
+  document.body.innerHTML = "<body><h1>String Generated</h1><br>" + string + "<br><br><button onclick='document.body.innerHTML = pageCode; delete pageCode;'>Restore page</button>";
   /* location.replace("https://shrayus-masanam.github.io/base64/?s=*"+decodedString); */ 
 }

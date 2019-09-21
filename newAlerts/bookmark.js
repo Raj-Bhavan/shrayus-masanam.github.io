5a31e06d482d87677d65b13c1021b8952dae0fad
shrayus-masanam <45981228+shrayus-masanam@users.noreply.github.com>
2019-09-21 15:21:46 -0400

    Update bookmark.js

--- newAlerts/bookmark.js
+++ newAlerts/bookmark.js
@@ -8,7 +8,7 @@ window.alert = function(message, color, textColor, borderSize, borderColor) {
 	if (borderSize == null) { borderSize = "5" }
 	if (borderColor == null) { borderColor = "black" }
 document.getElementById(`newAlert`).innerHTML = `<div class="alert" style="border:`+borderSize+`px solid `+borderColor+`; border-pixel-size: 10px; opacity: 0.99;transition: opacity 0.6s;padding: 20px;background-color:`+color+`;color: `+textColor+`;margin-bottom: 15px;margin-left: 30%;margin-right: 30%;">
-  <span onclick="document.getElementById('newAlert').innerHTML=''" class="closebtn" style="filter: invert(100%); margin-left: 15px;color: `+color+`;font-weight: bold;float: right;font-size: 22px;line-height: 20px;cursor: pointer;transition: 0.3s;">&times;</span>
+  <span onclick="document.getElementById('newAlert').innerHTML=''" class="closebtn" style="mix-blend-mode: difference; margin-left: 15px;color: `+color+`;font-weight: bold;float: right;font-size: 22px;line-height: 20px;cursor: pointer;transition: 0.3s;">&times;</span>
   <span id="discmsg" style="font-size: 15pt;">`+message+`</span>
 </div>`
 };

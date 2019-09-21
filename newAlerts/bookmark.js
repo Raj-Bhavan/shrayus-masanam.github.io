ae3aef280a217cf2183deda2fdbfcc028ea4fae7
shrayus-masanam <45981228+shrayus-masanam@users.noreply.github.com>
2019-09-21 15:39:59 -0400

    Update bookmark.js

--- newAlerts/bookmark.js
+++ newAlerts/bookmark.js
@@ -1,3 +1,18 @@
+function getHash(str) {
+    var hash = 0;
+    for (var i = 0; i < str.length; i++) {
+       hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    }
+    return hash;
+} 
+
+function convRGB(i){
+    var c = (i & 0x00FFFFFF)
+        .toString(16)
+        .toUpperCase();
+
+    return "00000".substring(0, 6 - c.length) + c;
+}
 var newAlert = document.createElement(`div`);
 newAlert.id = `newAlert`;
 document.body.appendChild(newAlert);
@@ -8,7 +23,7 @@ window.alert = function(message, color, textColor, borderSize, borderColor) {
 	if (borderSize == null) { borderSize = "5" }
 	if (borderColor == null) { borderColor = "black" }
 document.getElementById(`newAlert`).innerHTML = `<div class="alert" style="border:`+borderSize+`px solid `+borderColor+`; border-pixel-size: 10px; opacity: 0.99;transition: opacity 0.6s;padding: 20px;background-color:`+color+`;color: `+textColor+`;margin-bottom: 15px;margin-left: 30%;margin-right: 30%;">
-  <span onclick="document.getElementById('newAlert').innerHTML=''" class="closebtn" style="mix-blend-mode: difference; margin-left: 15px;color: white;font-weight: bold;float: right;font-size: 22px;line-height: 20px;cursor: pointer;transition: 0.3s;">&times;</span>
+  <span onclick="document.getElementById('newAlert').innerHTML=''" class="closebtn" style="margin-left: 15px;color: convRGB(getHash(color));font-weight: bold;float: right;font-size: 22px;line-height: 20px;cursor: pointer;transition: 0.3s;">&times;</span>
   <span id="discmsg" style="font-size: 15pt;">`+message+`</span>
 </div>`
 };

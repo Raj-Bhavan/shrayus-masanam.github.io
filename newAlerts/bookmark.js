b4c2fe972f6dcfa83410432c8547c680af75929f
Shrayus <shrayus@gmail.com>
2019-09-21 17:06:43 -0400

    Update bookmark.js

--- newAlerts/bookmark.js
+++ newAlerts/bookmark.js
@@ -13,14 +13,14 @@ function getHash(str) {
 var newAlert = document.createElement(`div`);
 newAlert.id = `newAlert`;
 document.body.appendChild(newAlert);
-window.alert = function(message, color, textColor, borderSize, borderColor) {
+window.alert = function(message, backgroundColor, textColor, borderSize, borderbackgroundColor) {
 	/* var rgb = [0,0,0];
-	var chosenColor; */
-	if (message == null) { message = "Correct format is alert(message, color, textColor, borderSize, borderColor);" }
-	if (color == null) {color = "white";}
+	var chosenbackgroundColor; */
+	if (message == null) { message = "Correct format is alert(message, backgroundColor, textColor, borderSize, borderbackgroundColor);" }
+	if (backgroundColor == null) {backgroundColor = "white";}
 	if (textColor == null) { textColor = "black" }
 	if (borderSize == null) { borderSize = "5" }
-	if (borderColor == null) { borderColor = "black" }
+	if (borderbackgroundColor == null) { borderbackgroundColor = "black" }
 	/* function convRGB(i){
     var c = (i & 0x00FFFFFF)
         .toString(16)
@@ -37,15 +37,15 @@ window.alert = function(message, color, textColor, borderSize, borderColor) {
 		rgb[1] = g;
 		rgb[2] = b;
 }
-	hexToRgb(convRGB(getHash(color)));
+	hexToRgb(convRGB(getHash(backgroundColor)));
 var o = Math.round(((parseInt(rgb[0]) * 299) + (parseInt(rgb[1]) * 587) + (parseInt(rgb[2]) * 114)) /1000);
     if(o > 125) {
-        chosenColor = `black`;
+        chosenbackgroundColor = `black`;
     }else{ 
-        chosenColor = `white`;
+        chosenbackgroundColor = `white`;
     } */
-document.getElementById(`newAlert`).innerHTML = `<div class="alert" style="border:`+borderSize+`px solid `+borderColor+`; border-pixel-size: 10px; opacity: 0.99;transition: opacity 0.6s;padding: 20px;background-color:`+color+`;color: `+textColor+`;margin-bottom: 15px;margin-left: 30%;margin-right: 30%;">
-  <span onclick="document.getElementById('newAlert').innerHTML=''" class="closebtn" style="margin-left: 15px;color: `+color+`;font-weight: bold;float: right;font-size: 22px;line-height: 20px;cursor: pointer;transition: 0.3s;">&times;</span>
+document.getElementById(`newAlert`).innerHTML = `<div class="alert" style="border:`+borderSize+`px solid `+borderbackgroundColor+`; border-pixel-size: 10px; opacity: 0.99;transition: opacity 0.6s;padding: 20px;background-backgroundColor:`+backgroundColor+`;backgroundColor: `+textColor+`;margin-bottom: 15px;margin-left: 30%;margin-right: 30%;">
+  <span onclick="document.getElementById('newAlert').innerHTML=''" class="closebtn" style="margin-left: 15px;color: `+textColor+`;font-weight: bold;float: right;font-size: 22px;line-height: 20px;cursor: pointer;transition: 0.3s;">&times;</span>
   <span id="discmsg" style="font-size: 15pt;">`+message+`</span>
 </div>`
 };

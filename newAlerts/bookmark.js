65147833357a6657aa65feb43c85f36844c8233e
shrayus-masanam <45981228+shrayus-masanam@users.noreply.github.com>
2019-09-23 12:26:48 -0400

    Update bookmark.js

--- newAlerts/bookmark.js
+++ newAlerts/bookmark.js
@@ -13,14 +13,14 @@ function getHash(str) {
 var newAlert = document.createElement(`div`);
 newAlert.id = `newAlert`;
 document.body.appendChild(newAlert);
-window.alert = function(message, backgroundColor, textColor, borderSize, borderbackgroundColor) {
+window.alert = function(message, backgroundColor, textColor, borderSize, borderColor) {
 	/* var rgb = [0,0,0];
 	var chosenbackgroundColor; */
 	if (message == null) { message = "Correct format is alert(message, backgroundColor, textColor, borderSize, borderbackgroundColor);" }
 	if (backgroundColor == null) {backgroundColor = "white";}
 	if (textColor == null) { textColor = "black" }
 	if (borderSize == null) { borderSize = "5" }
-	if (borderbackgroundColor == null) { borderbackgroundColor = "black" }
+	if (borderColor == null) { borderColor = "black" }
 	/* function convRGB(i){
     var c = (i & 0x00FFFFFF)
         .toString(16)
@@ -44,7 +44,7 @@ var o = Math.round(((parseInt(rgb[0]) * 299) + (parseInt(rgb[1]) * 587) + (parse
     }else{ 
         chosenbackgroundColor = `white`;
     } */
-document.getElementById(`newAlert`).innerHTML = `<div class="alert" style="border:`+borderSize+`px solid `+borderbackgroundColor+`; border-pixel-size: 10px; opacity: 0.99;transition: opacity 0.6s;padding: 20px;background-backgroundColor:`+backgroundColor+`;backgroundColor: `+textColor+`;margin-bottom: 15px;margin-left: 30%;margin-right: 30%;">
+document.getElementById(`newAlert`).innerHTML = `<div class="alert" style="border:`+borderSize+`px solid `+borderColor+`; border-pixel-size: 10px; opacity: 0.99;transition: opacity 0.6s;padding: 20px;background-backgroundColor:`+backgroundColor+`;backgroundColor: `+textColor+`;margin-bottom: 15px;margin-left: 30%;margin-right: 30%;">
   <span onclick="document.getElementById('newAlert').innerHTML=''" class="closebtn" style="margin-left: 15px;color: `+textColor+`;font-weight: bold;float: right;font-size: 22px;line-height: 20px;cursor: pointer;transition: 0.3s;">&times;</span>
   <span id="discmsg" style="font-size: 15pt;">`+message+`</span>
 </div>`

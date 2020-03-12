93f7c0e2e871716307d098ef0ab074c15601fc98
Shrayus Masanam <45981228+shrayus-masanam@users.noreply.github.com>
2020-03-12 19:38:31 -0400

    Update portal.js

--- standalone/portal.js
+++ standalone/portal.js
@@ -1,9 +1,12 @@
-document.head.innerHTML=``;
-document.body.innerHTML=`<input id="jsonNum"></input><button onclick='document.getElementById("dump").value="";var k = 0;while (k<11) {document.getElementById("dump").value=document.getElementById("dump").value+"\\n"+varsToMake[k]+document.getElementById("jsonNum").value+":        "+window[varsToMake[k]+document.getElementById("jsonNum").value];k++;}'>Get</button><br><textarea id="dump"></textarea>`;
+/* Aiming for a better MCPS PowerSchool */
+document.head.innerHTML=`<style>table,th,td {font-family:Helvetica;border:1px solid black;border-collapse:collapse;}</style>`;
+document.body.innerHTML=`<table id="gradeTable"><tr><td><strong>Course</strong></td><td><strong>Overall Grade</strong></td></tr></table>`;
+const oldHTML = `<input id="jsonNum"></input><button onclick='document.getElementById("dump").value="";var k = 0;while (k<11) {document.getElementById("dump").value=document.getElementById("dump").value+"\\n"+varsToMake[k]+document.getElementById("jsonNum").value+":        "+window[varsToMake[k]+document.getElementById("jsonNum").value];k++;}'>Get</button><br><textarea id="dump"></textarea>`
+attempt = true;
 var client = new XMLHttpRequest();
 client.open('GET', 'https://portal.mcpsmd.org/guardian/prefs/gradeByCourseSecondary.json?schoolid=823');
 client.onreadystatechange = function() {
-  var obj = JSON.parse(client.responseText);
+  obj = JSON.parse(client.responseText);
   var makeVarsFromResponse = function(n) {
     var i;
     for (i=0;i<Object.keys(obj).length;i++) {
@@ -15,6 +18,21 @@ client.onreadystatechange = function() {
   var j;
   for(j=0;j<Object.keys(varsToMake).length+1;j++) {
     makeVarsFromResponse(varsToMake[j]);
+  }
+  var m;
+  if (attempt==true) {
+    attempt = false;
+    for (m=Object.keys(obj).length;m>-1;m--) {
+      if (window["termid"+m] == `"MP3"`) {
+        console.log("Match!")
+        var table = document.getElementById("gradeTable");
+        var row = table.insertRow(1);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        cell1.innerHTML = window["courseName"+m];
+        cell2.innerHTML = window["overallgrade"+m];
+      }
+    }
   }
 }
 client.send();
